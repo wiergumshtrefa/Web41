d7da02e1593bdb8e06051725ab6a13b8e9f433dd
Kris <oaisjdfoijsf@ijdfoasj>
2024-11-07 21:33:49 +0300

    some changes in tasks

--- html/Lesson6/task10/script.js
+++ html/Lesson6/task10/script.js
@@ -1,5 +1,5 @@
 const prompt = require('prompt-sync')();
 let number1 = Number(prompt('Введите длину прямоугольника: '));
 let number2 = Number(prompt('Введите ширину прямоугольника: '));
-let perimeter = 2 * (number1 + number2)
-console.log("Периметр прямоугольника: " + perimeter)
+let perimeter = 2 * (number1 + number2);
+console.log("Периметр прямоугольника: " + perimeter);
