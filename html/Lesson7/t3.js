fa7407af148186da1ac53811b87b31a817f2c606
chris <chris@mypc.chris>
2024-11-14 20:56:04 +0300

    task3 for js is done

---
+++ html/Lesson7/t3.js
@@ -0,0 +1,11 @@
+function factorial(N) {
+    let result = 1;
+    for (let i = 1; i <= N; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+const prompt = require('prompt-sync')();
+let num = Number(prompt("Введите число N: "));
+console.log(factorial(num));
