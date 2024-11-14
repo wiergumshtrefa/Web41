a2951f428f04913e88d96dfc9092a5ecb9280d5d
chris <chris@mypc.chris>
2024-11-14 21:07:17 +0300

    task4 is done

---
+++ html/Lesson7/t4.js
@@ -0,0 +1,11 @@
+function findMax(arr) {
+    let max = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+console.log(findMax([3, 5, 7, 2, 8]));
