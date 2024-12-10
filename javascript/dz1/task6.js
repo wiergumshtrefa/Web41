bcc22fb190fe054fdcb97c2ffbc5450135beafcd
Kris <oaisjdfoijsf@ijdfoasj>
2024-12-10 21:34:17 +0300

    task7

--- javascript/dz1/task6.js
+++ javascript/dz1/task6.js
@@ -1,5 +1,5 @@
 const prompt = require('prompt-sync')();
 let dollar = prompt('введите количество долларов: ');
 const skolko = 0.95;
-let euro = dollar * 0.95;
+let euro = dollar * skolko;
 console.log(`столько доллров в евро ${euro}`);
