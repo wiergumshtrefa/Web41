d5f1433a37f2146709b16c45ee3cf42ca8f152a9
Kris <oaisjdfoijsf@ijdfoasj>
2024-12-10 21:01:25 +0300

    Катя сделала 3 задачу

--- javascript/dz1/task2.js
+++ javascript/dz1/task2.js
@@ -1,5 +1,5 @@
 const prompt = require('prompt-sync')();
-let birth = prompt("Введите свой год рождения: ");
+let name = prompt("Введите своё имя: ");
 const year = 2024;
 let result = year - birth;
 console.log(`Вам ${result} лет`)
