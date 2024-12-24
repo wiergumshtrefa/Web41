00dda6e27639924ef11b662904b8349199735700
Kris <oaisjdfoijsf@ijdfoasj>
2024-12-24 21:13:26 +0300

    федор жадаев сделал задание 6

---
+++ javascript/zadachi/l3/task6.js
@@ -0,0 +1,8 @@
+const prompt = require('prompt-sync')();
+let num = prompt('введите первоначальный обьем воды: ');
+let days = 0;
+while (num >= 10) {
+    num *= 0.9;
+    days ++;
+}
+console.log(days)
