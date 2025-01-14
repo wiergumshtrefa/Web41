74dd6994c7c1ebc8ccdfd9fdc008e3a303f67699
Kris <oaisjdfoijsf@ijdfoasj>
2025-01-14 19:37:43 +0300

    task1

--- javascript/dz4/task1.js
+++ javascript/dz4/task1.js
@@ -1,3 +1,19 @@
+const prompt = require('prompt-sync')();
 
+function compareNambers(num1, num2) {
+    if (num1 > num2) {
+        return 1;
 
+    } else if (num1 < num2) {
+        return -1;
+        
+    } else {
+        return 0;
+    }
+}
+
+let num1 = prompt('Введите первое число');
+let num2 = prompt('Введите второе число');
+const result = compareNambers(num1, num2);
+console.log(`результат сравнение: ${result}`);
 
