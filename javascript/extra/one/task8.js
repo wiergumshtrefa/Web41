85c059b99e9d91d7f7c9d92910843395baa96759
Kris <49696248+Kris465@users.noreply.github.com>
2025-01-16 17:41:33 +0000

    8е задание сделала Настя

---
+++ javascript/extra/one/task8.js
@@ -0,0 +1,7 @@
+const prompt = require('prompt-sync')();
+let num1 = prompt('введите 1-е число:');
+let num2 = prompt('введите 2-е число:');
+let num3 = prompt('введите 3-е число:');
+let num4 = prompt('введите 4-е число:');
+
+console.log(`числа: ${num1} ${num2} ${num3} ${num4}`);
