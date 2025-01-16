4bb1f69508346002422ada87fcf732baf03718d8
Kris <oaisjdfoijsf@ijdfoasj>
2025-01-16 20:54:02 +0300

    task11

---
+++ javascript/extra/one/task11.js
@@ -0,0 +1,7 @@
+const prompt = require('prompt-sync')();
+let num1 = prompt('введите 1-е число:');
+let num2 = prompt('введите 2-е число:');
+let num3 = prompt('введите 3-е число:');
+let num4 = prompt('введите 4-е число:');
+
+console.log(`числа:\n${num1}\n${num2}\n${num3}\n${num4}`);
