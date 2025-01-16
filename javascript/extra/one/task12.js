b2a72073941578d122c2fd6560dd9b106f433481
Kris <49696248+Kris465@users.noreply.github.com>
2025-01-16 18:09:19 +0000

    task12 done

---
+++ javascript/extra/one/task12.js
@@ -0,0 +1,10 @@
+const prompt = require('prompt-sync')();
+
+let t = prompt('введите 1-е число:');
+let v = prompt('введите 2-е число:');
+let x = prompt('введите 3-е число:');
+let y = prompt('введите 4-е число:');
+
+console.log(`a) 5 10 б) ${t} в) ${x}`)
+console.log(` 7 см  ${v}  ${x,y}`)
+
