079673ed21845751cce917599ff8fac78ae58549
Kris <49696248+Kris465@users.noreply.github.com>
2025-01-16 17:32:35 +0000

    задача 1.7

---
+++ javascript/extra/one/task7.js
@@ -0,0 +1,7 @@
+const prompt = require('prompt-sync')();
+let num1 = prompt('введите 1е число:')
+let num2 = prompt('введите 2е число:')
+let num3 = prompt('введите 3е число:')
+ 
+
+console.log(`числа: ${num1}  ${num2}  ${num3} `)
