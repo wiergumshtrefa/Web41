53c84db30d88293a867366d2c3f5279cfc425f46
Kris <oaisjdfoijsf@ijdfoasj>
2025-01-16 21:05:08 +0300

    катя сделала 13 задачу

---
+++ javascript/extra/one/task13.js
@@ -0,0 +1,9 @@
+const prompt = require('prompt-sync')();
+
+let a = prompt('введите a:');
+let b = prompt('введите b:');
+let x = prompt('введите x:');
+let y = prompt('введите y:');
+
+console.log(`a) 2 кг б) ${a} 1 в) ${x} ${y}`)
+console.log(`  13 17   19 ${b}    5 ${y}`)
