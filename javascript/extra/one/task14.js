0b0a6507ce58180c11a327394452cdede913835f
Kris <oaisjdfoijsf@ijdfoasj>
2025-01-16 21:31:08 +0300

    some changes

---
+++ javascript/extra/one/task14.js
@@ -0,0 +1,15 @@
+const prompt = require('prompt-sync')();
+
+let x = prompt('введите x: ');
+let sinx = prompt('введите sinx: ');
+let a = prompt('введите a:');
+let 
+let n = prompt('введите n: ');
+let sinA = prompt('введите sinA: ');
+let cosB = prompt('введите cosB: ');
+
+console.log(` a) 2x = ${2 * x}`);
+console.log(` б) sin x = ${Math.sin(x * Math.PI / 180)}`)
+console.log(` В) a^2 ${a * a}`)
+console.log(` г) sqrt(x) = ${Math.sqrt(x)}`)
+console.log(``)
