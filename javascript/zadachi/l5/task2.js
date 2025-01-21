7a4f9df2794d2a7f58b7208c4b91c059f6753122
Kris <oaisjdfoijsf@ijdfoasj>
2025-01-21 20:43:39 +0300

    сделал артем

---
+++ javascript/zadachi/l5/task2.js
@@ -0,0 +1,54 @@
+const prompt = require('prompt-sync')();
+
+function findMinimum(...args) {
+    return Math.min(...args);
+}
+
+function collectNumbers() {
+    const numbers = [];
+
+    while (true) {
+        const input = prompt('Введите число (или stop для завершения): ');
+
+        if (input.toLowerCase() === "stop") {
+            break; 
+        }
+
+        const number = parseFloat(input);
+
+        if (!isNaN(number)) {
+            numbers.push(number);
+        } else {
+            console.log("Введите коректное число или stop: ");
+
+        }
+    }
+
+    if(numbers.lengh > 0) {
+        console.log(`Минимальное значение: ${findMinimum(...numbers)}`);
+    } else {
+        console.log('Не было введено ни одного числа.');
+    }
+}
+
+
+collectNumbers();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
