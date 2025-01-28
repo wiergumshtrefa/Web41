8157e38aae56083066a74fa4433578ef734ac811
Kris <nataza7394@gmail.com>
2025-01-28 19:13:07 +0300

    катя сделала 31 номер

---
+++ javascript/extra/one/task31.js
@@ -0,0 +1,6 @@
+const prompt = require('prompt-sync')();
+let x = Number(prompt("введите значение х:"));
+let y = Number(prompt("введите значение y:"));
+
+console.log(` а) ( x + y ) / 2 = ${( x + y ) / 2}`)
+console.log(` б) ( x * y ) / 2 = ${( x * y ) / 2}`)
