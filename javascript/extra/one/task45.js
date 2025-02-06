905e05c04c0ee3600a471acc304d45f8cfb6f3cf
Kris465 <nataza7394@gmail.com>
2025-02-06 18:28:31 +0300

    some changes

---
+++ javascript/extra/one/task45.js
@@ -0,0 +1,20 @@
+const prompt = require('prompt-sync')();
+
+function pepsi(num1, num2) {
+    const sum = num1 + num2;
+    const difference = num1 - num2;
+    const product = num1 * num2;
+    const quotient = num2 !== 0 ? num1 / num2 : 'Деление на ноль невозможно';
+    
+    return { sum, difference, product, quotient };
+}
+
+const num1 = prompt("Введите первое число:");
+const num2 = prompt("Введите второе число:");
+
+const result = pepsi(num1, num2);
+
+console.log(`Сумма: ${result.sum}`);
+console.log(`Разность: ${result.difference}`);
+console.log(`Произведение: ${result.product}`);
+console.log(`Частное: ${result.quotient}`);
