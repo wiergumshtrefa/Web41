d145aabf1127c0e82c27beb74629baebd6be1979
Kris465 <nataza7394@gmail.com>
2025-02-13 20:33:00 +0300

    11 задача

---
+++ javascript/extra/two/task11.js
@@ -0,0 +1,12 @@
+const prompt = require('prompt-sync')();
+
+const num = parseInt(prompt("Введите двузначное число: "));
+
+if (num < 10 || num > 99) {
+    console.log("Ошибка: число должно быть двузначным.");
+} else {
+    const tens = Math.floor(num / 10);
+    const units = num % 10;       
+    const reversedNum = units * 10 + tens;
+    console.log(`Число, образованное при перестановке цифр: ${reversedNum}`);
+}
