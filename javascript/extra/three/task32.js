b59e72edb87e703d3036868dd78ec124d12df227
Kris465 <nataza7394@gmail.com>
2025-02-25 21:18:09 +0300

    фисташки

---
+++ javascript/extra/three/task32.js
@@ -0,0 +1,19 @@
+
+
+function checkPointInShadedArea(x, y) {
+    const inRectangle = (x >= 0 && x <= 5 && y >= 0 && y <= 5);
+    const inCircle = ((x - 3) ** 2 + (y - 3) ** 2 <= 2 ** 2);
+    
+    return {
+        inRectangle: inRectangle,
+        inCircle: inCircle,
+        inShadedArea: inRectangle || inCircle 
+    };
+}
+
+const prompt = require('prompt-sync')();
+const x = parseFloat(prompt('Введите координату x: '));
+const y = parseFloat(prompt('Введите координату y: '));
+
+const results = checkPointInShadedArea(x, y);
+console.log(results);
