97659afc4694973f65cd2ae841764ecea71a2d66
Kris465 <nataza7394@gmail.com>
2025-02-27 19:04:38 +0300

    чот сделал

---
+++ php/task10.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+echo "5\n";
+echo "10\n";
+echo "21\n";
+?>
+</body>
+</html>
