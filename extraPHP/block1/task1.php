776fb0d1b10b45a58720565d090602e4f7bc95a7
kavitosik <km220410@gmail.com>
2025-03-13 18:59:33 +0300

    задача 1 rework

--- extraPHP/block1/task1.php
+++ extraPHP/block1/task1.php
@@ -1,8 +1,3 @@
-TODO 
-<!-- Вычислить число π: Используйте встроенную константу M_PI, которая содержит значение числа π с высокой точностью.
-Округлить до сотых: Используйте функцию round() или number_format() для округления числа π до двух знаков после запятой.
-Вывести результат: Вставьте результат в HTML с помощью PHP. -->
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -12,6 +7,26 @@ TODO
     <title>Document</title>
 </head>
 <body>
-    <P>3,14</P>
+<?php
+
+$pi = M_PI;
+
+
+$roundedPi = round($pi, 2);
+
+
+echo "<!DOCTYPE html>
+<html lang='ru'>
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Число π</title>
+</head>
+<body>
+    <h1>Значение числа π, округлённое до сотых:</h1>
+    <p>$roundedPi</p>
+</body>
+</html>";
+?>
 </body>
 </html>
